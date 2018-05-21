237433be91333b666f370ff1048b1b1a48b605d9
iSan4eZ <temnyip@gmail.com>
2018-05-21 15:12:17 +0300

    Progect recreation

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '123',
+            'cookieValidationKey' => 'hFxOhIKX_SZBgr_8Qn_7Nxc6SHPlY0Hr',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
